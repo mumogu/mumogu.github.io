4a41125f54ea413c351293b965260beddd757fc7
Dirk Nettelnbreker <dirk@popskorn.de>
2020-12-01 13:24:47 +0100

    test

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 import { Foo } from 'foo.js'
 
 const f = new Foo('me');
+
+console.log('This is top-level');
+
+document.addEventListener('DOMContentLoaded', event => {
+    console.log('This is in event-listener');
+    const g = new Foo('you');
+})
